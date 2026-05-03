08ed4a0a0bb8497b8bfec2f0aedaf465394b3e97
Hans Preinfalk <hans.preinfalk.davila@gmail.com>
2026-05-03 16:23:16 -0700

    Initial Commit

---
+++ canopy/canopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  canopyApp.swift
+//  canopy
+//
+//  Created by Hans Preinfalk on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct canopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
