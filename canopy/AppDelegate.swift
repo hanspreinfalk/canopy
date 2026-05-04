bfdb7d81a0c46f7acf527e767a272952005b6258
Hans Preinfalk <hans.preinfalk.davila@gmail.com>
2026-05-03 23:10:45 -0700

    menu dock

--- canopy/AppDelegate.swift
+++ canopy/AppDelegate.swift
@@ -11,11 +11,39 @@ import HotKey
 class AppDelegate: NSObject, NSApplicationDelegate {
     var contentWindow: NSWindow?
     var hotKey: HotKey?
+    var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.accessory)
+        NSApp.setActivationPolicy(.regular)
+        setupStatusItem()
         showContentWindow()
         setupHotkey()
+    }
+
+    func applicationDockMenu(_ sender: NSApplication) -> NSMenu? {
+        let menu = NSMenu()
+        menu.addItem(withTitle: "Open Canopy", action: #selector(openCanopy), keyEquivalent: "")
+        menu.addItem(.separator())
+        menu.addItem(withTitle: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "")
+        return menu
+    }
+
+    private func setupStatusItem() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem?.button {
+            button.image = NSImage(systemSymbolName: "leaf.fill", accessibilityDescription: "Canopy")
+        }
+
+        let menu = NSMenu()
+        menu.addItem(withTitle: "Open Canopy", action: #selector(openCanopy), keyEquivalent: "")
+        menu.addItem(.separator())
+        menu.addItem(withTitle: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        statusItem?.menu = menu
+    }
+
+    @objc private func openCanopy() {
+        NSApp.activate(ignoringOtherApps: true)
+        showContentWindow()
     }
 
     private func setupHotkey() {
